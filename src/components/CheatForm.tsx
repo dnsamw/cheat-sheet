9e76fee0b6ce5486c34c3ca77d5b4b42b5c0d898
dnsamw <dnsam.mit@gmail.com>
2024-08-11 15:19:47 +0530

    css update

--- src/components/CheatForm.tsx
+++ src/components/CheatForm.tsx
@@ -1,45 +1,58 @@
-import { useForm, FieldValues, Controller, useFieldArray, Control } from "react-hook-form";
-import { z } from "zod";
+import { useEffect } from "react";
+import {
+  useForm,
+  FieldValues,
+  Controller,
+  useFieldArray,
+  Control,
+} from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import Alert, { AlertTypes } from "./UI/Alert";
-import { useData } from "../hooks/useData";
+import { z } from "zod";
+
+import Spinner from "./UI/Spinner";
 import TagSelector from "./TagSelector";
-import { LuPlusCircle, LuXCircle } from "react-icons/lu";
+import Alert, { AlertTypes } from "./UI/Alert";
 import PostEditorTest from "./Experimental/PostEditorTest";
+import { useData } from "../hooks/useData";
+import { LuPlusCircle, LuXCircle } from "react-icons/lu";
 import { useModal } from "../contexts/modalContext";
 import { ModalMethods } from "../types/modal";
 
 import "../assets/scss/cheat-form.scss";
-import Spinner from "./UI/Spinner";
-import { useEffect } from "react";
-
 
 type Props = {};
 
 const schema = z.object({
   title: z.string().min(1),
   text: z.string().min(1),
-  codes: z.array(z.string().min(1)).min(1, "At least one code snippet is required"),
+  codes: z
+    .array(z.string().min(1))
+    .min(1, "At least one code snippet is required"),
   tags: z.array(z.string()).min(1, "Select at least one tag"),
 });
 
 type FormData = z.infer<typeof schema>;
 
 function CheatForm({}: Props) {
-  const { createCheatItem, updateCheatItem,loading } = useData();
-  const {state:{item,method}} = useModal();
+  const { createCheatItem, updateCheatItem, loading } = useData();
+  const {
+    state: { item, method },
+  } = useModal();
   const {
     register,
     handleSubmit,
     control,
     formState: { errors },
-    reset
-  } = useForm<FormData>({ resolver: zodResolver(schema),defaultValues: {
-    title: item?.title || '',
-    text: item?.text || '',
-    codes: item?.codes || ["enter code here"],
-    tags: item?.tags || [],
-  }, });
+    reset,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      title: item?.title || "",
+      text: item?.text || "",
+      codes: item?.codes || ["enter code here"],
+      tags: item?.tags || [],
+    },
+  });
 
   const { fields, append, remove } = useFieldArray({
     control: control as Control<FormData>,
@@ -59,7 +72,7 @@ function CheatForm({}: Props) {
           codes: data.codes,
           tags: data.tags,
         });
-        break
+        break;
       case ModalMethods.CREATE:
         await createCheatItem({
           title: data.title,
@@ -77,16 +90,14 @@ function CheatForm({}: Props) {
     console.log("CHEAT FORM mounted");
     return () => {
       console.log("CHEAT FORM unmounted");
-    }
-  }, [])
+    };
+  }, []);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)} className="four">
       <div className="input-unit">
         <label htmlFor="title">Title</label>
-        <div>
-
-        </div>
+        <div></div>
         <input type="text" {...register("title")} />
         {errors.title && (
           <Alert type={AlertTypes.error} message={errors.title.message} />
@@ -94,48 +105,51 @@ function CheatForm({}: Props) {
       </div>
 
       <div className="input-unit">
-      <label htmlFor="title">Text</label>
-      <Controller
+        <label htmlFor="title">Text</label>
+        <Controller
           name="text"
           control={control}
           defaultValue={""}
           render={({ field }) => (
-            <PostEditorTest value={field.value}
-                    onChange={field.onChange} />
+            <PostEditorTest value={field.value} onChange={field.onChange} />
           )}
         />
         {errors.text && (
           <Alert type={AlertTypes.error} message={errors.text.message} />
         )}
       </div>
-
-      <div className="dynamic-input-control-unit">
-        Code Snippets
-      <span className="action-btn" onClick={() => append('')}><LuPlusCircle /></span>
-      </div>
       {fields.map((field, index) => (
         <div className="input-unit" key={field.id}>
           <label htmlFor={`codes.${index}`}>Code {index + 1}</label>
-          <textarea {...register(`codes.${index}` as  const)} />
+          <textarea className="code" {...register(`codes.${index}` as const)} />
           {errors.codes?.[index] && (
-            <Alert type={AlertTypes.error} message={errors.codes[index]?.message} />
+            <Alert
+              type={AlertTypes.error}
+              message={errors.codes[index]?.message}
+            />
           )}
           {index > 0 && (
-            <span onClick={() => remove(index)}><LuXCircle /></span>
+            <span className="code-close-btn" onClick={() => remove(index)}>
+              <LuXCircle />
+            </span>
           )}
         </div>
       ))}
 
+      <div className="dynamic-input-control-unit">
+      Code Snippets
+        <span className="action-btn" onClick={() => append("")}>
+          <LuPlusCircle />
+        </span>
+      </div>
+
       <div className="input-unit">
-      <Controller
+        <Controller
           name="tags"
           control={control}
           defaultValue={[]}
           render={({ field }) => (
-            <TagSelector
-              value={field.value}
-              onChange={field.onChange}
-            />
+            <TagSelector value={field.value} onChange={field.onChange} />
           )}
         />
         {errors.tags && (
